8d6989cdc8bf15761ae8043989dbb346c3797818
Sanyukkie <78085479+Sanyukkie@users.noreply.github.com>
2021-02-02 19:31:46 +0200

    Create task.php

---
+++ task.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>task 3</title>
+</head>
+<body>
+<?php
+// Task 1
+$month = date(F);
+if ($month == "August"){
+    echo "It's August, so it's still holiday. <br>";
+}
+else {
+echo "Not August, This is $month so i don't have any holidays <br>";
+}
+
+// Task 2
+
+$color = "red";
+if ($color == "red")
+echo "The color is red. <br>" ;
+else
+echo "The color is not red. <br>";
+
+
+// Task 3
+
+$grade = 100;
+
+if ($grade > 80 && $grade <= 100)
+echo "Excelent!";
+else if ($grade > 70 && $grade < 80)
+echo "Great";
+else if ($grade > 60 && $grade < 70)
+echo "Good";
+else if ($grade > 50 && $grade < 60)
+echo "Pass";
+else if ($grade < 50)
+echo "Fail";
+
+?>
+
+
+
+
+
+<form name="form1" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+Name: <input type="text" name="fname"> <br>
+Age: <input type="text" name="age"> <br>
+<input type="submit" value="Submit Data">
+</form>
+
+<?php
+$age = $_POST["age"];
+if ($age >= 18)
+echo "You are eligible to vote<br>";
+else
+echo "You are not eligible to vote<br>";
+
+// Task 5
+
+for ($i=8; $i>0; $i--) {
+echo substr("12345678", 0, $i);
+echo "<br>";
+}
+
+// Task 6
+$i=1;
+do{
+    echo substr("********", 0, $i);
+    echo "<br>";
+    $i++;
+} while($i<9);
+
+
+
+?>
+
+
+
+
+
+
+    
+</body>
+<html>
