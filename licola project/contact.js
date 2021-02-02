d93ce3aac50fa9cdefd526779d0bc91bbe3644af
Aleksander Truss <78085479+Sanyukkie@users.noreply.github.com>
2021-02-02 19:54:16 +0200

    Add files via upload

---
+++ licola project/contact.js
@@ -0,0 +1,6 @@
+document.querySelector(".form-container .btn").addEventListener("click", () =>{
+    document.querySelector(".send").style.display = "block";
+  })
+  document.querySelector(".sendcontent .btn").addEventListener("click", () =>{
+    document.querySelector(".send").style.display = "none";
+  })
