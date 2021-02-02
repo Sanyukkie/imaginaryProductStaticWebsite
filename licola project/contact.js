8bcb20f8ce8aed2481f01b4e0a7ddc8416b441b0
Aleksander Truss <78085479+Sanyukkie@users.noreply.github.com>
2021-02-02 20:01:35 +0200

    Delete licola project directory

--- licola project/contact.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".form-container .btn").addEventListener("click", () =>{
-    document.querySelector(".send").style.display = "block";
-  })
-  document.querySelector(".sendcontent .btn").addEventListener("click", () =>{
-    document.querySelector(".send").style.display = "none";
-  })
